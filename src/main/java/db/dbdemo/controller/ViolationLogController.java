a0cc960b18099033506d44a979021d69e2a843e5
Tamashii90 <patoboy90@hotmail.com>
2021-09-22 19:34:04 +0300

    ** implement native query for faster execution **

--- src/main/java/db/dbdemo/controller/ViolationLogController.java
+++ src/main/java/db/dbdemo/controller/ViolationLogController.java
@@ -1,14 +1,15 @@
 package db.dbdemo.controller;
 
-import db.dbdemo.model.*;
+import db.dbdemo.model.Vehicle;
+import db.dbdemo.model.VehicleViolationKey;
+import db.dbdemo.model.Violation;
+import db.dbdemo.model.ViolationLog;
 import db.dbdemo.repository.VehiclesRepo;
 import db.dbdemo.repository.ViolationsLogRepo;
 import db.dbdemo.repository.ViolationsRepo;
-import org.hibernate.boot.model.naming.Identifier;
-import org.hibernate.id.IdentifierGenerationException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -42,24 +43,15 @@ public class ViolationLogController {
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
     public void addViolationOccurred(@Validated @RequestBody ViolationLog violationLog) {
-        Vehicle vehicle;
-        Violation violation;
         String plugedNumber = violationLog.getId().getPlugedNumber();
         long violationId = violationLog.getId().getViolationId();
-        VehicleViolationKey vehicleViolationKey = new VehicleViolationKey(plugedNumber, violationId);
-
-        if (violationsLogRepo.existsById(vehicleViolationKey)) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Violation Log already exists.");
-        }
+        String location = violationLog.getLocation();
+        boolean paid = violationLog.isPaid();
         try {
-            vehicle = vehiclesRepo.findById(plugedNumber).get();
-            violation = violationsRepo.findById(violationId).get();
-        } catch (NoSuchElementException e) {
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "No such vehicle/violation");
+            violationsLogRepo.insertLog(plugedNumber, violationId, location, paid);
+        } catch (DataIntegrityViolationException e) {
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid plugedNumber/violationId");
         }
-        violationLog.setViolation(violation);
-        violationLog.setVehicle(vehicle);
-        violationsLogRepo.save(violationLog);
     }
 
     @PutMapping
--- src/main/java/db/dbdemo/repository/ViolationsLogRepo.java
+++ src/main/java/db/dbdemo/repository/ViolationsLogRepo.java
@@ -2,10 +2,18 @@ package db.dbdemo.repository;
 
 import db.dbdemo.model.VehicleViolationKey;
 import db.dbdemo.model.ViolationLog;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ViolationsLogRepo extends CrudRepository<ViolationLog, VehicleViolationKey> {
-
+    @Transactional
+    @Modifying
+    @Query(value = "INSERT INTO violations_log(pluged_number, violation_id, date, location, paid) VALUES(:plugedNumber, :violationId, CURRENT_DATE, :location, :paid)", nativeQuery = true)
+    void insertLog(@Param("plugedNumber") String plugedNumber, @Param("violationId") long violationId,
+                   @Param("location") String location, @Param("paid") boolean paid);
 }
